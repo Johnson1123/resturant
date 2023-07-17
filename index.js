8c84cdb1da240c5d5f495bd87b0b81531644650e
Johnson1123 <onifadejohnson2017@gmail.com>
2023-07-17 11:45:10 -0700

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 const port = 3002;
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("heloo world");
